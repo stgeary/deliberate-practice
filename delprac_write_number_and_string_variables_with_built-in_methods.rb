76817f33c3d0de62bb38fc6ea3a9484fa946c35c
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-24 11:43:33 -0600

    Add files via upload

---
+++ delprac_write_number_and_string_variables_with_built-in_methods.rb
@@ -0,0 +1,58 @@
+#1. Write a program that uses a variable to store a number, then prints that number plus 10.
+
+a = 5
+b = a +10
+puts b
+
+#2. Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+c = "spooky scary skeletons"
+puts c.upcase
+
+#3. Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+d = 17
+e = 38
+f = d + e
+puts f
+
+#4. Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+g = "Scott Geary"
+h = "tacocat"
+puts g.reverse
+puts h.reverse
+
+#5. Write a program that uses a variable to store a number, then prints the number times 10.
+
+i = 7
+j = i * 10
+puts j
+
+#6. Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+k = "Ya like "
+l = "Jazz?"
+puts k + l
+
+
+#7. Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+m = "Antidisestablishmentarianism"
+puts m.length
+
+#8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+n = 3.1420328520493587432657432895647326975138763475693257935413986537896543
+puts n.to_i
+
+#9. Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+o = [0,8]
+q = o[1] * o[0]
+puts q
+
+
+#10. Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+r = "SPOOKY SCARY SKELETONS"
+puts r.downcase
